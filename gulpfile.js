b50455c37fb3d4eaffa686737fafab7124c8bab7
Louis Ritchie <louiscritchie@gmail.com>
2017-04-15 16:43:50 -0700

    Gulp copies public & compiles jade to build, fixes src paths; ignore build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var replace = require('gulp-replace');
 
-/**
- * Compile jade files into /public directory
- */
-gulp.task('jade', function() {
+gulp.task('copy-public', function() {
+    return gulp.src("public/**")
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('compile-jade', function() {
     return gulp.src(["views/*.jade", "!views/error.jade"])
         .pipe(jade())
-        .pipe(gulp.dest('public'));
+		.pipe(replace('src="/', 'src="./'))
+        .pipe(gulp.dest('build'));
 });
 
 /**
- * Default task, running just `gulp` will compile the jade files into /public
+ * Default task:
+ * Copy public dir to build dir
+ * Compile jade into build dir
  */
-gulp.task('default', ['jade']);
+gulp.task('default', ['copy-public', 'compile-jade']);
